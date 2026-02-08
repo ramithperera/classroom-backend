5d8d812e33e99738052b45418b6d4a2b084ff709
Ramith <89182652+ramithperera@users.noreply.github.com>
2026-02-08 13:21:40 +0530

    implement database schemas

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
@@ -9,6 +9,6 @@ app.get('/', (req , res) => {
     res.send('Welcome to Classroom Backend API!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
 })
