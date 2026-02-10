47934e41f313fef16e1c238bdbea676fa91626b6
Ramith <89182652+ramithperera@users.noreply.github.com>
2026-02-10 13:26:27 +0530

    Implement GET subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import cors from "cors";
+import subjectsRouter from "./routes/subjects";
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
+
 app.use(express.json());
+
+app.use('/api/subjects', subjectsRouter)
 
 app.get('/', (req , res) => {
     res.send('Welcome to Classroom Backend API!');
